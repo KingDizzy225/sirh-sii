efd92218009cca7a2e8dcde3230534094215b146
KingDizzy225 <diopibrahim225@gmail.com>
2026-05-11 18:46:04 +0000

    fix: remove debug ErrorBoundary from main.jsx to resolve Vercel build error

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ErrorBoundary } from './ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 )
