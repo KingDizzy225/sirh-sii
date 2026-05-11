f9cbaa9213507a8ab5cd09c22f3528027de12697
KingDizzy225 <diopibrahim225@gmail.com>
2026-05-11 17:52:57 +0000

    fix: remove virtual:pwa-register import causing blank page in production

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { registerSW } from 'virtual:pwa-register'
-
-// Enregistrement automatique du Service Worker pour la PWA
-registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
